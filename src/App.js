76a46535fc563de6b8cbf1f49b8c0ca342592161
Ignat Tokar <example@exapmle.com>
2022-06-18 14:04:21 +0300

    realized base functionality for exchanging

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_KEY } from './key';
 
 function App() {
+
+  const [USD, setUSD] = useState(0);
+  const [UAH, setUAH] = useState(0);
+  const [EUR, setEUR] = useState(0);
+
+  useEffect(()=>{
+    getConvertResult("USD", "UAH", USD); 
+    getConvertResult("USD", "EUR", USD); 
+  }, [USD]);
+
+  // useEffect(()=>{
+  //   getConvertResult("UAH", "USD", UAH); 
+  //   getConvertResult("UAH", "EUR", UAH); 
+  // }, [UAH]);  
+
+  // useEffect(()=>{
+  //   getConvertResult("EUR", "USD", EUR); 
+  //   getConvertResult("EUR", "UAH", EUR); 
+  // }, [EUR]);   
+
+  const getConvertResult = (from, to, amount) => {
+    axios
+      .get(`https://api.apilayer.com/currency_data/convert?from=${from}&to=${to}&amount=${amount}`, {
+        headers: {
+          'apikey': API_KEY
+        }
+      })
+      .then(response => {
+        switch (to) { 
+          case "USD": 
+            setUSD(response.data.result);
+            break;
+          case "UAH": 
+            setUAH(response.data.result);
+            break;       
+          case "EUR": 
+            setEUR(response.data.result);
+            break;                  
+          default:
+            break;
+        }
+      })
+  }
+
+  const changeHandlerUAH = (e) => setUAH(e.target.value);
+  const changeHandlerUSD = (e) => setUSD(e.target.value);
+  const changeHandlerEUR = (e) => setEUR(e.target.value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>USD</h2>
+        <input 
+          type="number" 
+          placeholder="USD"
+          value={USD}
+          onChange={changeHandlerUSD}
+        />
+        <h2>UAH</h2>
+        <input 
+          type="number" 
+          placeholder="UAH"
+          value={UAH}
+          onChange={changeHandlerUAH}
+        />
+        <h2>EUR</h2>
+        <input 
+          type="number" 
+          placeholder="EUR"
+          value={EUR}
+          onChange={changeHandlerEUR}
+        />        
       </header>
     </div>
   );
