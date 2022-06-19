caf62906635b5dc8a06df3aa85bcc133cc270fa6
Ignat Tokar <example@example.com>
2022-06-19 17:52:12 +0300

    realize dry principle for api.js

--- src/components/api.js
+++ src/components/api.js
@@ -2,16 +2,8 @@ import axios from "axios";
 import { roundFunc } from "./utitlities";
 
 export const getStartInfo = (setEurInfo, setUsdInfo) => {
-  axios
-    .get('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/uah.json')
-    .then(data => {
-      setEurInfo(roundFunc(data.data.uah));
-    });
-  axios
-    .get('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/uah.json')
-    .then(data => {
-      setUsdInfo(roundFunc(data.data.uah));
-    })
+  setStartInfo('eur', setEurInfo);
+  setStartInfo('usd', setUsdInfo);
 }
 
 export const convert = (selectFrom, selectTo, amount, setState, setIs) => {
@@ -21,4 +13,12 @@ export const convert = (selectFrom, selectTo, amount, setState, setIs) => {
       setState(roundFunc(data.data[selectTo] * amount));
       setIs(false);
     });
+}
+
+const setStartInfo = (currency, setState) => {
+  axios
+    .get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}/uah.json`)
+    .then(data => {
+      setState(roundFunc(data.data.uah));
+    });  
 }
